4b71a7fc2dfb582059c598413412bc0f9cf6360b
ahasanbegovic <hasanbegovic.arman@gmail.com>
2019-10-18 16:10:19 +0200

    add delay code

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# guitar-pedal
-A dsPIC-based guitar pedal.
+# dspic-guitar-pedal
+A dsPIC-based guitar pedal. It is built around a 16-bit DSP microprocessor. The project is designed to showcase some common practices in digital signal processing, PCB design and embedded programming. The A/D and D/A conversions are done using 12-bits, so the lowpass filters are designed to take away a lot of the higher frequencies, which results in a quite unique and dark sound of the pedal when it is on.
 
 ## Circuit
 
-The circuit is designed using Altium Designer v17.1.5 Build 472. It is based around a breakout board for the dsPIC60F6014A-PF microcontroller designed and sold by Mikroelektronika (see Resources for documentation). Third party models sourced from [SnapEDA](https://www.snapeda.com/home/). All the project files (including the third party models) can be found in the `circuit` directory.
+The circuit is designed using `Altium Designer v17.1.5 Build 472`. It is based around a breakout board for the dsPIC60F6014A-PF microcontroller designed and sold by Mikroelektronika (see Resources for documentation). Third party models sourced from [SnapEDA](https://www.snapeda.com/home/). All the project files (including the third party models) can be found in the `circuit` directory. The schematic of the circuit is given below.
+
+![Schematic](circuit/figures/schematic.png)
+
+A two-layer PCB is designed and documented. The top and bottom layers, as well as a preview of the component layout, are given below.
+
+![Top layer](circuit/figures/top_layer.png) ![Bottom layer](circuit/figures/bottom_layer.png) ![Assembly](circuit/figures/assembly.png)
+
+Industry-standard output files can be found in `circuit/Project Outputs for GuitarPedal`.
 
 ## Code
 
-The code is written using Mikroelektronika mikroC PRO for dsPIC v7.1.0. The dsPIC is programmed using a USB programmer designed and sold by Mikroelektronika (see Resources for documentation). The template of the code is written in C and given in the `code` directory.
+The code is written using Mikroelektronika `mikroC PRO for dsPIC v7.1.0`. The dsPIC is programmed using a USB programmer designed and sold by Mikroelektronika (see Resources for documentation). The essential code (`template.c`) is written in C and given in the `code` directory. An example of using that code to create a delay effect is given in `delay.c`.
 
 ## Resources
 
---
+++ code/delay.c
@@ -0,0 +1,136 @@
+const char CS_PIN = 1;
+const char LD_PIN = 2;
+const char AN10_PIN = 10;
+const char RBx_PIN = 8;
+const char IF_PIN = 0;
+
+#define max_buffer_size 4000
+
+unsigned int delay_buffer[max_buffer_size];
+unsigned int buffer_size = max_buffer_size;
+unsigned int iterator = 0;
+
+void ADC_Init_manual(){
+     ADPCFG = 0;
+     ADCON1 = 0x0004;
+     ADCHS = 0x000A;
+     ADCSSL = 0; //No scan
+     ADCON3 = 0x0F00; //TAD = internalTCY
+     ADCON3 = 0x1003; //ADCS=5 (min TAD for 10MHz is 3*TCY=300ns)
+     ADCON2 = 0;
+     ADCON1.F15 = 1;
+     ADCON1.F9 = 1;
+     ADCON1.F8 = 0; // Unsigned fractional
+}
+
+int ADC_Read_manual(unsigned int channel){
+     ADCHS = channel;
+     ADCON1.F1 = 0; //Clear SAMP bit (trigger conversion)
+     while(ADCON1.F1 == 0) {
+         asm nop; //Wait for DONE bit in ADCON1
+     }
+     return ADCBUF0;
+}
+
+void InitMain() {
+    int i;
+    for(i = 0; i < max_buffer_size; i++){
+       delay_buffer[i] = 2048;
+    }
+    ADPCFG = 0xFBFF; //10th channel is sampled and coverted
+    TRISB.AN10_PIN = 1;  // Set AN0 as input
+    TRISB.RBx_PIN = 0;  // Set RBx as output
+    LATB.RBx_PIN = 0;
+    //ADC1_Init();
+    ADC_Init_Manual();
+
+    // Spi2_Init(); // Initialize SPI2 module
+    SPI2_Init_Advanced(_SPI_MASTER, // SPI in Master mode
+                         _SPI_8_BIT, // 8-bit data
+                         _SPI_PRESCALE_SEC_1, _SPI_PRESCALE_PRI_1, //prescaler = 1
+                         _SPI_SS_DISABLE,
+                         _SPI_DATA_SAMPLE_MIDDLE,
+                         _SPI_CLK_IDLE_LOW,
+                         _SPI_ACTIVE_2_IDLE //Falling edge
+                        );
+
+    TRISC.CS_PIN = 0; // Set CS pin as output
+    TRISC.LD_PIN = 0; // Set LD pin as output
+
+
+    T1IE_bit = 1;         //timer1
+    IPC0 = IPC0 | 0x1000; //highest priority
+    T1IF_bit = 0;         //first interrupt
+    T1CON = 0x8000;       // Internal clock (20 Mhz), prescaler 1:1
+    PR1= 226; // Period of Timer1 226 (interrupt every 22.6us, 44.1kHz sampling)
+}//~
+
+
+void DAC_Output(unsigned int valueDAC) {
+    unsigned char temp;
+
+    LATC.CS_PIN = 0; // Select DAC module
+    LATC.LD_PIN = 0; // Enable data transfer
+
+
+
+    // Send 2 bytes of valueDAC variable
+    temp = (valueDAC >> 8) & 0x0F; // Prepare hi-byte for transfer
+    // It's a 12-bit number, so only
+    // lower nibble of high byte is used
+    temp |= 0x30; // Set MCP4921 control bits
+    Spi2_Write(temp); // Send data via SPI
+
+
+
+    temp = valueDAC; // Prepare lo-byte for transfer
+    Spi2_Write(temp); // Send data via SPI
+
+    LATC.LD_PIN = 1; // Disable data transfer
+    LATC.CS_PIN = 1; // Deselect DAC module
+}//~
+
+void Timer1Int() iv IVT_ADDR_T1INTERRUPT {
+    int analog_data;
+    int output_data;
+    int pot_value;
+
+    LATB.RBx_PIN = ~LATB.RBx_PIN;
+
+    pot_value = (ADC_Read_Manual(0x000B) >> 1) & 0xFFF0;
+    if (pot_value > 16384){
+       pot_value = 16384;
+    }
+
+
+    analog_data = ADC_Read_Manual(0x000A);
+    W4 = analog_data;
+    W5 = delay_buffer[iterator];
+    W6 = 32767 - pot_value; 
+    W7 = pot_value;
+    CORCON.IF_PIN = 0;
+    asm{
+        clr A
+        mac W4*W6, A, W4, W4, W6, W6
+        sac A, #0, W1
+        clr B
+        mac W5*W7, B, W5, W5, W7, W7
+        sac B, #0, W2
+    }
+    output_data = W1 + W2;
+    CORCON.IF_PIN = 1;
+    delay_buffer[iterator++] = output_data;
+
+
+    if (iterator >= buffer_size) iterator = 0;
+
+    DAC_Output(((output_data) >> 4) & 0x0FFF); 
+
+    T1IF_bit = 0;
+}
+
+void main() {
+ InitMain();
+ while (1);
+}//~!
+
